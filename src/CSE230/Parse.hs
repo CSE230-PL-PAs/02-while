5321f9b847b61c1a8a76d886bf0876fc37b7a684
Z <tzang.xiao@gmail.com>
2022-11-15 16:47:07 -0800

    problem 2 solved

--- src/CSE230/Parse.hs
+++ src/CSE230/Parse.hs
@@ -3,7 +3,6 @@ module CSE230.Parse where
 import Text.Parsec hiding (State, between)
 import Text.Parsec.String
 import qualified CSE230.Types as H
-import CSE230.Types (Bop(Plus))
 
 {- As you can see, it is rather tedious to write the above tests! 
    They correspond to the code in the files `test.imp` and `fact.imp`. 
@@ -12,8 +11,8 @@ import CSE230.Types (Bop(Plus))
    for the WHILE language from the previous problem.
 -}
 
-parseFromString :: Parser a -> String -> Either ParseError a 
-parseFromString p s = runParser p () "DUMMY" s 
+parseFromString :: Parser a -> String -> Either ParseError a
+parseFromString p s = runParser p () "DUMMY" s
 
 -- >>> parseFromString varP "X45"
 -- Right "X"
@@ -39,7 +38,9 @@ intP = do
 -- Next, define a parser that will accept a particular string `s` as a given value `x`
 
 constP :: String -> a -> Parser a
-constP s x = return x
+constP s x = do
+               _ <- string s
+               return x
 
 -- Use the above to define a parser for boolean values 
 -- where `"true"` and `"false"` should be parsed appropriately.
@@ -72,7 +73,34 @@ varP = many1 upper
 -- operators are right associative, and they all have the same precedence.
 
 exprP :: Parser H.Expression
-exprP   = error "fill this in"
+exprP   = choice [try exprOp, exprVal, exprVar]
+
+exprVal :: Parser H.Expression
+exprVal = do
+            H.Val <$> valueP
+
+exprVar :: Parser H.Expression
+exprVar = do
+            H.Var <$> varP
+
+exprOp :: Parser H.Expression
+exprOp = do
+            x <- exprVar <|> exprVal <|> parenthesesP exprOp
+            spaceP
+            o <- opP
+            spaceP
+            y <- exprVar <|> exprVal <|> parenthesesP exprOp
+            return (H.Op o x y)
+
+spaceP :: Parser ()
+spaceP = skipMany space
+
+parenthesesP :: Parser p -> Parser p
+parenthesesP p = do
+                    string "("
+                    r <- p
+                    string ")"
+                    return r
 
 -------------------------------------------------------------------------------
 -- | Parsing Statements 
@@ -81,7 +109,66 @@ exprP   = error "fill this in"
 -- Next, use the expression parsers to build a statement parser
 
 statementP :: Parser H.Statement
-statementP = error "fill this in"
+statementP = choice [try sequenceP, assignP, ifP, whileP, skipP]
+
+assignP :: Parser H.Statement
+assignP = do
+            spaceP
+            x <- varP
+            spaceP
+            string ":="
+            spaceP
+            H.Assign x <$> exprP
+
+ifP :: Parser H.Statement
+ifP = do
+            spaceP
+            string "if"
+            spaceP
+            e <- exprP
+            spaceP
+            string "then"
+            spaceP
+            s1 <- statementP
+            spaceP
+            string "else"
+            spaceP
+            s2 <- statementP
+            spaceP
+            string "endif"
+            spaceP
+            return (H.If e s1 s2)
+
+whileP :: Parser H.Statement
+whileP = do
+            spaceP
+            string "while"
+            spaceP
+            e <- exprP
+            spaceP
+            string "do"
+            spaceP
+            s <- statementP
+            spaceP
+            string "endwhile"
+            spaceP
+            return (H.While e s)
+
+sequenceP :: Parser H.Statement
+sequenceP = do
+              spaceP
+              s1 <- assignP <|> ifP <|> whileP <|> skipP
+              string ";"
+              spaceP
+              H.Sequence s1 <$> statementP
+
+
+skipP :: Parser H.Statement
+skipP = do
+            spaceP
+            string "skip"
+            return H.Skip
+
 
 -- When you are done, we can put the parser and evaluator together 
 -- in the end-to-end interpreter function `runFile` in `Main.hs`
